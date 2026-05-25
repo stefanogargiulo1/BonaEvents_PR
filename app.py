ffe820308d8a69f7b819ca7fd3cf4e9d98bebd2e
Stefano Gargiulo <stefanogariulo1@gmail.com>
2026-05-25 21:11:07 +0200

    clean fix

--- app.py
+++ app.py
@@ -1,14 +1,7 @@
 from flask import Flask
-import os
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return "BonaEvents PR Panel Online 🔥"
-
-# Railway usa questa variabile automaticamente
-port = int(os.environ.get("PORT", 8080))
-
-# IMPORTANTISSIMO:
-app.run(host="0.0.0.0", port=port)
