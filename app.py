b88c47d4a4d720a18a5d8f1451f8182f4882becd
Stefano Gargiulo <stefanogariulo1@gmail.com>
2026-05-25 21:15:32 +0200

    added login ui

--- app.py
+++ app.py
@@ -3,5 +3,9 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def home():
+def login():
+    return render_template("login.html")
+
+@app.route("/dashboard")
+def dashboard():
     return render_template("index.html")
