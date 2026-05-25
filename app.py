f6c278913ada55d0f0b0dbdf853c9d9c24867ed8
Stefano Gargiulo <stefanogariulo1@gmail.com>
2026-05-25 21:13:38 +0200

    added ui

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "BonaEvents PR Panel Online 🔥"
+    return render_template("index.html")
