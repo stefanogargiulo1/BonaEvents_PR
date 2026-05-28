29a5e45658572604a3e9ac87d2a6089661fbb3c3
Stefano Gargiulo <stefanogariulo1@gmail.com>
2026-05-28 03:06:53 +0200

    fix3

--- app.py
+++ app.py
@@ -70,9 +70,11 @@ app = Flask(__name__)
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USE_SSL'] = False
 
 app.config['MAIL_USERNAME'] = 'bonaeventsapp@gmail.com'
 app.config['MAIL_PASSWORD'] = 'jfjv xhut fxgb jckk'
+app.config['MAIL_DEFAULT_SENDER'] = 'bonaeventsapp@gmail.com'
 
 mail = Mail(app)
 def send_ticket_email(
